a01e6caa31cb531efff28493d82a52fc1890e00c
Zurol <zurolk@gmail.com>
2021-01-26 22:37:48 -0600

    Corrección de bug al imprimir.

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import os
 
 def imprimeFibonacci():
 
-    tope = int(input("\n\nIntroduce el número máximo para la secuencia."))
+    tope = int(input("\n\nIntroduce el número máximo para la secuencia. "))
 
     print("\n\n Ahora introduce el rango que no va a mostrarse")
     min = int(input("Introduce el número min:"))
     max = int(input("Introduce el número max:"))
-
-
 
     numeroFib = 0
     numeroViejo = 0
@@ -17,7 +15,7 @@ def imprimeFibonacci():
     while (numeroFib <= tope):
         #print ("numeroFib{0} - {1}".format(numeroFib, numeroViejo))
 
-        if ( not (numeroFib > min and numeroFib < max ) ):
+        if ( not (numeroFib >= min and numeroFib <= max ) ):
             print ("{0}".format(numeroFib, numeroViejo))
 
         if numeroFib == 0:
